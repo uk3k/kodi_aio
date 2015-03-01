f05de4520acbe6f62aae3ce9802f8f0afe915171
Paul Krause <github@uk3k.de>
2015-03-01 11:10:16 +0100

    Update packages.sh

--- install_scripts/packages.sh
+++ install_scripts/packages.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 #ensure system is up to date
+apt-add-repository ppa:team-xbmc/ppa -y
 apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 
-#add ppas
-apt-add-repository ppa:team-xbmc/ppa -y
+#define packages to install
+basepkg
+gfxpkg
+kodipkg
+mysqlpkg
+vdrpkg
+oscampkg
+pyloadpkg
+
