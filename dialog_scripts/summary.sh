fbb36c0a82ded7c466e52059e1b4342ab12ff171
Paul Krause <github@uk3k.de>
2015-03-28 13:18:36 +0100

    Update summary.sh

--- dialog_scripts/summary.sh
+++ dialog_scripts/summary.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+##define variables
+
+#system type
+$sys_type
+
+#media auto mount
+$media_mount
+
+#networking
+$nw_ip
+$nw_netmask
+$nw_gateway
+$nw_dns1
+$nw_dns2
+
+#mysql
+$sql_rootpw
+$sql_userpw
+
+#live-tv
+$tv_vdr
+$tv_dvb_type
+$tv_oscam
+$tv_prefcg_oscam
+$tv_cardreader
+$tv_scan
+
+#pyload
+$dl_pyload
+
