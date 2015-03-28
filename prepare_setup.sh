4fd6766d24320e701c94a9ec6a7c4c8180fb0413
Paul Krause <github@uk3k.de>
2015-03-28 15:55:51 +0100

    Update prepare_setup.sh

--- prepare_setup.sh
+++ prepare_setup.sh
@@ -12,11 +12,11 @@ if [ "$(lsb_release -c | awk '{ print $2 }')" != "trusty" ]
 		exit 2
 fi
 
-#exit if networking is not working
-if [ -z "$(ping -c 3 github.com |grep time &>/dev/null)" ]
+#exit if github is not reachable
+if [ -n "$(ping -c 3 github.com |grep time &>/dev/null)" ]
 	then
 		echo "Github.com is not reachable, please check your internet connection or try again later."
-		echo "This script can't be executed as long Github.com is not reacheable"
-		echo "or neither your internet connection isn't working"
+		echo "This script can't be used as long Github.com is not reachable"
+		echo "or neither your internet connection isn't working correctly."
 		exit 3	
 fi
