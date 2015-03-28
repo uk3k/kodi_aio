008c7cf9d46d10105de849ccdbd1e0abc25ea788
Paul Krause <github@uk3k.de>
2015-03-28 16:51:36 +0100

    Update defaults.sh

--- dialog_scripts/defaults.sh
+++ dialog_scripts/defaults.sh
@@ -6,27 +6,27 @@ setup="default"             #default, custom
 
 #system
 sys_type="host"             #host, client
-. $hw/gfx_detection  #run gfx_detection.sh to determine the actual graphics vendor of the system
-$sys_fgx                    #amd, nvidia, intel, other
-. $hw/os_detection.sh  #run os_detection.sh to determine the actual operating system
-$sys_os                     #the actual operating system
+. $hw/gfx_detection.sh      #run gfx_detection.sh to determine the actual graphics vendor of the system
+sys_fgx="$sys_fgx"          #amd, nvidia, intel, other
+. $hw/os_detection.sh       #run os_detection.sh to determine the actual operating system
+sys_os="$sys_os"            #the actual operating system
 
 #media auto mount
 media_mount="false"         #false, true
 
 #networking
 #assume networking is working as we were using netinstall
-. $hw/network_detection.sh     #run network_detection.sh to get *value* for each variable
-$nw_iface                   #detected primary network interface; if $nw_iface="eth*" AND $wifi_present="true"--> prompt; else keep interface
-$nw_wifi_present            #detected wifi state; if $nw_wifi_present="true" and $nw_iface!="wlan*" --> prompt, else false
-$nw_use_wifi                #false, true; see $nw_wifi_present                
-$nw_wifi_ssid               #detected wifi ssid; if $nw_use_wifi=true --> prompt; else unset
-$nw_wifi_psk                #detected wifi psk; if $nw_use_wifi=true --> prompt; else unset
-$nw_ip                      #detected ip-address
-$nw_netmask                 #detected netmask
-$nw_gateway                 #detected gateway
-$nw_dns1                    #detected nameserver #1          
-$nw_dns2                    #detected nameserver #2
+. $hw/network_detection.sh          #run network_detection.sh to get *value* for each variable
+nw_iface="$nw_iface"                #detected primary network interface; if $nw_iface="eth*" AND $wifi_present="true"--> prompt; else keep interface
+nw_wifi_present="$nw_wifi_present"  #detected wifi state; if $nw_wifi_present="true" and $nw_iface!="wlan*" --> prompt, else false
+nw_use_wifi="$nw_use_wifi"          #false, true; see $nw_wifi_present                
+nw_wifi_ssid="$nw_wifi_ssid"        #detected wifi ssid; if $nw_use_wifi=true --> prompt; else unset
+nw_wifi_psk="$nw_wifi_psk"          #detected wifi psk; if $nw_use_wifi=true --> prompt; else unset
+nw_ip="$nw_ip"                      #detected ip-address
+nw_netmask="$nw_netmask"            #detected netmask
+nw_gateway="$nw_gateway"            #detected gateway
+nw_dns1="$nw_dns1"                  #detected nameserver #1          
+nw_dns2="$nw_dns2"                  #detected nameserver #2
 
 #mysql
 sql_ip="$nw_ip"             #ip address of the lokal mysql-server
