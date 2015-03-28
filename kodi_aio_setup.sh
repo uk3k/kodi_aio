0a49041c68b93c895cef0b980401fd2b11112e94
Paul Krause <github@uk3k.de>
2015-03-28 16:44:29 +0100

    Update kodi_aio_setup.sh

--- kodi_aio_setup.sh
+++ kodi_aio_setup.sh
@@ -2,5 +2,5 @@
 #Version v1.0.0.0 Alpha
 #use this one as management script to call all modules
 
-. dialog_scripts/defaults.sh
-. dialog_scripts/summary.sh
+. /tmp/dialog_scripts/defaults.sh
+. /tmp/dialog_scripts/summary.sh
