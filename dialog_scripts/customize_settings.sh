034d6a777bbd6b744076f924fb3ee9f317a17254
Paul Krause <github@uk3k.de>
2015-03-29 14:06:00 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -4,47 +4,61 @@
 #define functions for customizations
 #system
 function customize-system {
- . $dialog/system/graphics_select.sh        #system settings: graphics vendor
+ . $dialog/system/graphics_select.sh        	#system settings: graphics vendor
+}
+
+#mysql
+function customize-mysql {
+ . $dialog/mysql/root_password.sh        	#mysql settings: root password for mysql
+ . $dialog/mysql/user_password.sh		#mysql settings: user password for kodi-database
 }
 
 #networking
 function customize-networking {
-. $dialog/networking/iface_select.sh        #network settings: primary networking interface
-. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
-. $dialog/networking/ip_settings.sh         #network settings: ip address
-. $dialog/networking/netmask_settings.sh    #network settings: netmask
-. $dialog/networking/gateway_settings.sh    #network settings: gateway
-. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
-. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
-. $dialog/networking/wifi_settings.sh       #network settings: wifi
+. $dialog/networking/iface_select.sh        	#network settings: primary networking interface
+. $dialog/networking/network_settings.sh    	#network settings: DHCP/Static
+. $dialog/networking/ip_settings.sh         	#network settings: ip address
+. $dialog/networking/netmask_settings.sh    	#network settings: netmask
+. $dialog/networking/gateway_settings.sh    	#network settings: gateway
+. $dialog/networking/dns1_settings.sh       	#network settings: Nameserver #1
+. $dialog/networking/dns2_settings.sh       	#network settings: Nameserver #2
+. $dialog/networking/wifi_settings.sh       	#network settings: wifi
 }
 
 #live-tv
 function customize-livetv {
-. $dialog/live_tv/vdr_select.sh
-}
+. $dialog/live_tv/vdr_select.sh			#live-tv settings: enable live-tv-support
+. $dialog/live_tv/dvb-type_select.sh		#live-tv settings: select dvb-type
+. $dialog/live_tv/scan_channels_select.sh	#live-tv settings: scan channels after setup
+. $dialog/live_tv/oscam_select.sh		#live-tv settings: enable paytv descrambling support
+. $dialog/live_tv/card-reader_select.sh		#live-tv settings: select cardreader for oscam
+. $dialog/live_tv/precfg_oscam_select.sh	#live-tv settings: preconfigure paytv-provider for oscam
+} 
 
 #additional stuff
 function customize-additional {
- #do something
+. $dialog/additional/pyload_select.sh		#additional stuff settings: install pyload download manager
 }
 
 #print selection menu
 whiptail --backtitle "$headline" \
         --title "Select Settings to customize" \
-        --checklist --separate-output "\nSelect the settings you want to customize \n\n " 30 100 4 \
+        --checklist --separate-output "\nSelect the settings you want to customize \n\n " 30 100 5 \
                 "System"		""	on     \
+                "MySQL"			""	on     \
                 "Networking"            ""	on      \
                 "Live-TV"               ""	on      \
                 "Additional Stuff"      ""	on      2>selections
 while read input
 do
 	case $input in
-		System) customize-system
+		System) 	customize-system
 		;;
-		Networking) customize-networking
+		MySQL) 		customize-mysql
+		;;
+		Networking) 	customize-networking
                 ;;        
-                Live-TV) customize-livetv
+                Live-TV) 	customize-livetv
                 ;;
                 Additional Stuff) customize-additional
 		*)
