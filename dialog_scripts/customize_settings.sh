ce63bd1161bee8e02e8578d28357a656fc99a781
Paul Krause <github@uk3k.de>
2015-03-29 13:53:11 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -20,63 +20,37 @@ function customize-networking {
 }
 
 #live-tv
-#function customize-livetv {
- #do something
-#}
+function customize-livetv {
+. $dialog/live_tv/vdr_select.sh
+}
 
-#dvb-type
-#function customize-dvbtype {
+#additional stuff
+function customize-additional {
  #do something
-#}
-
-#oscam
-#function customize-oscam {
- #do something
-#}
-
-#card-reader
-#function customize-cardreader {
- #do something
-#}
-
-#pyload
-#function customize-pyload {
- #do something
-#}
+}
 
 #print selection menu
 whiptail --backtitle "$headline" \
         --title "Select Settings to customize" \
-        --checklist --separate-output "\nSelect the settings you want to customize \n\n " 30 100 7 \
-                "Graphics-Vendor"       ""	off     \
-                "Networking"            ""	off      \
-                "Live-TV"               ""	off      \
-                "DVB-Type"              ""	off      \
-                "PayTV-Support"         ""	off      \
-                "Card-Reader"           ""	off      \
-                "Pyload"                ""	off      2>selections
+        --checklist --separate-output "\nSelect the settings you want to customize \n\n " 30 100 4 \
+                "System"		""	on     \
+                "Networking"            ""	on      \
+                "Live-TV"               ""	on      \
+                "Additional Stuff"      ""	on      2>selections
 while read input
 do
 	case $input in
-		Graphics-Vendor) customize-system
+		System) customize-system
 		;;
 		Networking) customize-networking
                 ;;        
+                Live-TV) customize-livetv
+                ;;
+                Additional Stuff) customize-additional
 		*)
 		;;
 	esac
 done < selections
-
-#networking
-#. $dialog/networking/iface_select.sh        #network settings: primary networking interface
-#. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
-#. $dialog/networking/ip_settings.sh         #network settings: ip address
-#. $dialog/networking/netmask_settings.sh    #network settings: netmask
-#. $dialog/networking/gateway_settings.sh    #network settings: gateway
-#. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
-#. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
-#. $dialog/networking/wifi_settings.sh       #network settings: wifi
-
 
 #print summary
 . $dialog/summary.sh
