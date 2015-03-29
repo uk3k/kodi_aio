b1868b3f000b1a60a2d91796e0f013d23521913b
Paul Krause <github@uk3k.de>
2015-03-29 14:12:28 +0200

    Test OK!
    Test OK!

--- dialog_scripts/networking/gateway_settings.sh
+++ dialog_scripts/networking/gateway_settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Gateway for TCP/IP configuration
+#V1.0.0.0.A
 
 unset valid
 while [ -z "$nw_gateway" ] || [ -z "$valid_range" ] || [ -z "$valid" ] || [ "$valid_syntax" != "true" ];do
