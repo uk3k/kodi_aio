e9fca6f7a51976757a1e5fdf62ef40fdb65f15a9
Paul Krause <github@uk3k.de>
2015-03-29 12:53:04 +0200

    Update graphics_select.sh

--- dialog_scripts/system/graphics_select.sh
+++ dialog_scripts/system/graphics_select.sh
@@ -7,7 +7,7 @@ input=`whiptail --backtitle "$headline" \
         Supported Vendors are:
         Intel:   Every iGPU from Chipsets/CPUs since 2007 (Intel GMA 8xx; Intel HD2xxx; ...) 
 	AMD:     Every AMD/ATI (i)GPU since HD5xxx (HD5xxx; HD6xxx; E350; E450; E1200; ...)
-	NVIDIA:  Every NVIDIA (i)GPU since GeForce 2xx (GF2xx; GF3xx; GF4xx; ION; ...) \n\n " 30 100 2 \
+	NVIDIA:  Every NVIDIA (i)GPU since GeForce 2xx (GF2xx; GF3xx; GF4xx; ION; ...) \n\n " 30 100 3 \
                 "Intel"     ""	\
                 "AMD"       ""  \
                 "NVIDIA"       ""	3>&1 1>&2 2>&3`
