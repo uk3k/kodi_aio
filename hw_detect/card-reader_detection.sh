ae37e2149f27fb6172cebe59bd17ecd06877f6c4
Paul Krause <github@uk3k.de>
2015-03-29 14:38:34 +0200

    Test OK!
    Test OK!

--- hw_detect/card-reader_detection.sh
+++ hw_detect/card-reader_detection.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #determine the selected smart-card-reader's usb-port
 #works actually only for smargo
+#V1.0.0.0.A
 
 if [ $tv_cardreader = "smargo" ] 
   then
-    tv_cr_busid=$(lsusb | grep ^Future.* | awk '{print $1}') 
-  esle
+    tv_cr_busid=$(lsusb | grep "Future Technology" | awk -v OFS=':' '{print $2,$4}' | sed s/://2) 
+  else
     tv_cr_busid="000:000"
 fi
