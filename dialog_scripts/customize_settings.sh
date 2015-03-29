abe3bcf887a8aa2493e4cb5005af37a10638434b
Paul Krause <github@uk3k.de>
2015-03-29 13:27:01 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -5,26 +5,45 @@
 input=`whiptail --backtitle "$headline" \
         --title "Select Settings to customize" \
         --menu "\nSelect the settings you want to customize \n\n " 30 100 2 \
-                "Graphics Vendor"        ""	\
-                "Live-TV"        ""	\
-                "DVB-Type"        ""	\
-                "PayTV Support"      "" \
-                "Card-Reader"      "" \
-                "Pyload"      ""	3>&1 1>&2 2>&3`
+                "Graphics-Vendor"       ""      \
+                "Networking"            ""      \
+                "Live-TV"               ""      \
+                "DVB-Type"              ""      \
+                "PayTV-Support"         ""      \
+                "Card-Reader"           ""      \
+                "Pyload"                ""      3>&1 1>&2 2>&3`
 selections=$input
 
-#system
-. $dialog/system/graphics_select.sh        #system settings: graphics vendor
+#systemwhile read choice
+while read selections
+do
+	case $selections in
+		Graphics-Vendor) 
+		        . $dialog/system/graphics_select.sh        #system settings: graphics vendor
+		;;
+		Networking)
+                        . $dialog/networking/iface_select.sh        #network settings: primary networking interface
+                        . $dialog/networking/network_settings.sh    #network settings: DHCP/Static
+                        . $dialog/networking/ip_settings.sh         #network settings: ip address
+                        . $dialog/networking/netmask_settings.sh    #network settings: netmask
+                        . $dialog/networking/gateway_settings.sh    #network settings: gateway
+                        . $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
+                        . $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
+                        . $dialog/networking/wifi_settings.sh       #network settings: wifi
+		*)
+		;;
+	esac
+done
 
 #networking
-. $dialog/networking/iface_select.sh        #network settings: primary networking interface
-. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
-. $dialog/networking/ip_settings.sh         #network settings: ip address
-. $dialog/networking/netmask_settings.sh    #network settings: netmask
-. $dialog/networking/gateway_settings.sh    #network settings: gateway
-. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
-. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
-. $dialog/networking/wifi_settings.sh       #network settings: wifi
+#. $dialog/networking/iface_select.sh        #network settings: primary networking interface
+#. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
+#. $dialog/networking/ip_settings.sh         #network settings: ip address
+#. $dialog/networking/netmask_settings.sh    #network settings: netmask
+#. $dialog/networking/gateway_settings.sh    #network settings: gateway
+#. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
+#. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
+#. $dialog/networking/wifi_settings.sh       #network settings: wifi
 
 
 #print summary
