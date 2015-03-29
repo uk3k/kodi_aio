f477fd565cdcf2140e38792efa46055d512ac62b
Paul Krause <github@uk3k.de>
2015-03-29 11:36:22 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#customize default settings
+
+#Network settings DHCP/static
+input=`whiptail --backtitle "$headline" \
+        --title "Network Settings" \
+        --radiolist "\nSelect if you want to use static or DHCP TCP/IP settings. \n\nNote: It's highly recommended to use static TCP/IP settings for the host! \n\n " 15 100 2 \
+                "Static"        ""      on      \
+                "DHCP"          ""      off     3>&1 1>&2 2>&3`
+nw_mode=$input
+
+#
