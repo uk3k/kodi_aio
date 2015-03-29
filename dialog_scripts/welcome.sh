4b8f923874a467c46660d74b67ee22455681751a
Paul Krause <github@uk3k.de>
2015-03-29 12:22:22 +0200

    Update welcome.sh

--- dialog_scripts/welcome.sh
+++ dialog_scripts/welcome.sh
@@ -13,15 +13,15 @@ whiptail --backtitle "$headline" \
 input=`whiptail --backtitle "$headline" \
         --title "Setup mode" \
         --menu "\nDo you want use the default setup settings or customize them? \n\nMost important default settings are: \n
-        	Graphics Vendor:		$sys_gfx
-		Network Interface:		$nw_iface
-		IP-Address Settings:		$nw_mode
-		IP-Address:			$nw_ip
-		Live-TV Support:		$tv_vdr
-		DVB-* Type:			dvb-$tv_dvb_type
-		Install OSCAM SoftCam:		$tv_oscam
-		Smart-Card-Reader:		$tv_cardreader
-		Pyload Download Manager:	$add_pyload \n\n " 30 100 2 \
+        	Graphics Vendor: 		$sys_gfx
+		Network Interface: 		$nw_iface
+		IP-Address Settings: 		$nw_mode
+		IP-Address: 			$nw_ip
+		Live-TV Support: 		$tv_vdr
+		DVB-* Type: 			dvb-$tv_dvb_type
+		Install OSCAM SoftCam: 		$tv_oscam
+		Smart-Card-Reader: 		$tv_cardreader
+		Pyload Download Manager: 	$add_pyload \n\n " 30 100 2 \
                 "Default"        ""	\
                 "Customize"      ""	3>&1 1>&2 2>&3`
 setup=$input
