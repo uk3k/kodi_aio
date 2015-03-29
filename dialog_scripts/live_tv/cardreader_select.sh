0fa639404aaa5ca3626a6d9f86f96f2a883750dd
Paul Krause <github@uk3k.de>
2015-03-29 15:03:58 +0200

    Update cardreader_select.sh

--- dialog_scripts/live_tv/cardreader_select.sh
+++ dialog_scripts/live_tv/cardreader_select.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+#select card-reader for oscam
+
+input=`whiptail --backtitle "$headline" \
+        --title "Smart-Card-Reader" \
+        --menu "\nSelect your Card-Reader for OSCAM \n\n " 30 100 2 \
+                "smargo"           ""	\
+                "smartreader"      ""	3>&1 1>&2 2>&3`
+tv_cardreader=$input
