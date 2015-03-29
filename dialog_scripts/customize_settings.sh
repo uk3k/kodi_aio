4e402d16a22644656aafcd60ae9ae911dcd530ec
Paul Krause <github@uk3k.de>
2015-03-29 12:09:53 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 #customize default settings
 
-#network settings: primary networking interface
-. $dialog/networking/iface_select.sh
-
-#network settings: DHCP/Static
-. $dialog/networking/network_settings.sh
-
-#network settings: ip address
-. $dialog/networking/ip_settings.sh
-
-#network settings: netmask
-. $dialog/networking/netmask_settings.sh
-
-#network settings: gateway
-. $dialog/networking/gateway_settings.sh
-
-#network settings: Nameserver #1
-. $dialog/networking/dns1_settings.sh
-
-#network settings: Nameserver #2
-. $dialog/networking/dns2_settings.sh
-
-#network settings: wifi
-. $dialog/networking/wifi_settings.sh
+#networking
+. $dialog/networking/iface_select.sh        #network settings: primary networking interface
+. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
+. $dialog/networking/ip_settings.sh         #network settings: ip address
+. $dialog/networking/netmask_settings.sh    #network settings: netmask
+. $dialog/networking/gateway_settings.sh    #network settings: gateway
+. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
+. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
+. $dialog/networking/wifi_settings.sh       #network settings: wifi
 
 
 #print summary
