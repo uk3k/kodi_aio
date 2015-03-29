32ee113638a575148f1276c0a62766677fd8652f
Paul Krause <github@uk3k.de>
2015-03-29 11:47:07 +0200

    Update gateway_settings.sh

--- dialog_scripts/networking/gateway_settings.sh
+++ dialog_scripts/networking/gateway_settings.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-#Netmask for TCP/IP configuration
+#Gateway for TCP/IP configuration
 
 unset valid
-while [ -z "$nw_netmask" ] || [ -z "$valid_range" ] || [ -z "$valid" ] || [ "$valid_syntax" != "true" ];do
+while [ -z "$nw_gateway" ] || [ -z "$valid_range" ] || [ -z "$valid" ] || [ "$valid_syntax" != "true" ];do
         input=`whiptail --backtitle "$headline" \
-        --title "TCP/IP configuration - Netmask" \
-        --inputbox "\nConfirm or enter the netmask for the host's TCP/IP configuration \n\n " 15 100 "$nw_netmask" 3>&1 1>&2 2>&3`
-        nw_netmask=$input
-        if [ -n "$nw_netmask" ]
+        --title "TCP/IP configuration - Gateway" \
+        --inputbox "\nConfirm or enter the gateway for the host's TCP/IP configuration \n\n " 15 100 "$nw_gateway" 3>&1 1>&2 2>&3`
+        nw_gateway=$input
+        if [ -n "$nw_gateway" ]
         then
-        valid_range="$(echo $nw_netmask | awk -F'.' '$1 >=1 && $1 <=255 && $2 >=0 && $2 <=255 && $3 >=0 && $3 <=255 && $4 >=0 && $4 <=255 && $5 <0')"
-    	  valid_syntax="$(echo $nw_netmask | rev | cut -c 1 )"
-		case "$valid_syntax" in
-		        [0-9] ) valid_syntax=true;;
-            * )     valid_syntax=false;;
-		esac
-    		if [ -z "$valid_range" ] || [ "$valid_syntax" != "true" ]
-    		then
+                valid_range="$(echo $nw_gateway | awk -F'.' '$1 >=1 && $1 <=255 && $2 >=0 && $2 <=255 && $3 >=0 && $3 <=255 && $4 >=0 && $4 <=255 && $5 <0')"
+                valid_syntax="$(echo $nw_gateway | rev | cut -c 1 )"
+                case "$valid_syntax" in
+        	        [0-9] )		valid_syntax=true;;
+                        * ) 		valid_syntax=false;;
+                esac
+                if [ -z "$valid_range" ] || [ "$valid_syntax" != "true" ]
+                then
                 whiptail --backtitle "$headline" \
                 --title "Wrong input" \
                 --msgbox "\n
-                Range or syntax incorrect. \nCorrect syntax is something like xxx.xxx.xxx.xxx \n\nPlease try again" 15 100
-                unset nw_netmask
+                IP-range or syntax incorrect. \nCorrect syntax is something like xxx.xxx.xxx.xxx \n\nPlease try again" 15 100
+                unset nw_gateway
                 else
                 valid="ok"
                 fi
@@ -30,6 +30,6 @@ while [ -z "$nw_netmask" ] || [ -z "$valid_range" ] || [ -z "$valid" ] || [ "$va
                 --title "Wrong input" \
                 --msgbox "\n
                 You didn't enter anything :-( \n\nPlease try again" 15 100
-                unset nw_netmask
+                unset nw_gateway
         fi
 done
