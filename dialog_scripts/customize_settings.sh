9f307a52ce2070e8430f935be93e1c3cbf95306e
Paul Krause <github@uk3k.de>
2015-03-29 13:34:25 +0200

    Update customize_settings.sh

--- dialog_scripts/customize_settings.sh
+++ dialog_scripts/customize_settings.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 #customize default settings
 
-#select settings to customize
+define functions for customizations
+#system
+function customize-system {
+ . $dialog/system/graphics_select.sh        #system settings: graphics vendor
+}
+
+#networking
+function customize-networking {
+. $dialog/networking/iface_select.sh        #network settings: primary networking interface
+. $dialog/networking/network_settings.sh    #network settings: DHCP/Static
+. $dialog/networking/ip_settings.sh         #network settings: ip address
+. $dialog/networking/netmask_settings.sh    #network settings: netmask
+. $dialog/networking/gateway_settings.sh    #network settings: gateway
+. $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
+. $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
+. $dialog/networking/wifi_settings.sh       #network settings: wifi
+}
+
+#live-tv
+function customize-livetv {
+ #do something
+}
+
+#dvb-type
+function customize-dvbtype {
+ #do something
+}
+
+#oscam
+function customize-oscam {
+ #do something
+}
+
+#card-reader
+function customize-cardreader {
+ #do something
+}
+
+#pyload
+function customize-pyload {
+ #do something
+}
+
+#print selection menu
 input=`whiptail --backtitle "$headline" \
         --title "Select Settings to customize" \
         --checklist "\nSelect the settings you want to customize \n\n " 30 100 7 \
@@ -18,18 +61,10 @@ selections=$input
 while read selections
 do
 	case $selections in
-		Graphics-Vendor) 
-		        . $dialog/system/graphics_select.sh        #system settings: graphics vendor
+		Graphics-Vendor) customize-system
 		;;
-		Networking)
-                        . $dialog/networking/iface_select.sh        #network settings: primary networking interface
-                        . $dialog/networking/network_settings.sh    #network settings: DHCP/Static
-                        . $dialog/networking/ip_settings.sh         #network settings: ip address
-                        . $dialog/networking/netmask_settings.sh    #network settings: netmask
-                        . $dialog/networking/gateway_settings.sh    #network settings: gateway
-                        . $dialog/networking/dns1_settings.sh       #network settings: Nameserver #1
-                        . $dialog/networking/dns2_settings.sh       #network settings: Nameserver #2
-                        . $dialog/networking/wifi_settings.sh       #network settings: wifi
+		Networking) customize-networking
+                ;;        
 		*)
 		;;
 	esac
