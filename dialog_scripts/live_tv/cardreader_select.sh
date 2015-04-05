9812db27403ee8e72801899dd21c9d407f958da9
Paul Krause <github@uk3k.de>
2015-04-05 12:17:14 +0200

    Update cardreader_select.sh

--- dialog_scripts/live_tv/cardreader_select.sh
+++ dialog_scripts/live_tv/cardreader_select.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 #select card-reader for oscam
-
-input=`whiptail --backtitle "$headline" \
-        --title "Smart-Card-Reader" \
-        --menu "\nSelect your Card-Reader for OSCAM \n\n " 30 100 2 \
-                "smargo"           ""	\
-                "smartreader"      ""	3>&1 1>&2 2>&3`
-tv_cardreader=$input
-
-if [ "$tv_cr_busid" = "000:000" ]
+if [ "$tv_vdr" = "true" ] && [ "$tv_oscam" = "true" ]
         then
-                if (whiptail --backtitle "$headline" \
-                --title "Card-Reader not found" \
-                --yesno "\nSetup couldn't find your Card-Reader automatically. \nDo you want to select it manually? \n\n " 15 100)
-        then
-                inputok="false"
+        input=`whiptail --backtitle "$headline" \
+                --title "Smart-Card-Reader" \
+                --menu "\nSelect your Card-Reader for OSCAM \n\n " 30 100 2 \
+                        "smargo"           ""	\
+                        "smartreader"      ""	3>&1 1>&2 2>&3`
+        tv_cardreader=$input
+
+        if [ "$tv_cr_busid" = "000:000" ]
+                then
+                        if (whiptail --backtitle "$headline" \
+                        --title "Card-Reader not found" \
+                        --yesno "\nSetup couldn't find your Card-Reader automatically. \nDo you want to select it manually? \n\n " 15 100)
+                then
+                        inputok="false"
+                else
+                        inputok="true"
+                        while [ "$inputok" = "false" ]; do
+                                read -ra array <<<$(lsusb)
+                                input=`whiptail --backtitle "$headline" \
+                                --notags \
+                                --title "Available USB-Devices" \
+                                --menu "\nSelect your Card-Reader \n " 16 100 4 "${array[@]}" 3>&1 1>&2 2>&3`
+                                nw_iface=$input
+                                if [ -z "$input" ]
+                                        then
+                                                whiptail --backtitle "$headline" \
+                                                --title "Wrong input" \
+                                                --msgbox "\nNothing selected :-(. \n\nPlease try again" 15 100
+                                                inputok="false"
+                                        else
+                                                inputok="true"
+                                fi
+                        done
         else
-                inputok="true"
-                while [ "$inputok" = "false" ]; do
-                        read -ra array <<<$(lsusb)
-                        input=`whiptail --backtitle "$headline" \
-                        --notags \
-                        --title "Available USB-Devices" \
-                        --menu "\nSelect your Card-Reader \n " 16 100 4 "${array[@]}" 3>&1 1>&2 2>&3`
-                        nw_iface=$input
-                        if [ -z "$input" ]
-                                then
-                                        whiptail --backtitle "$headline" \
-                                        --title "Wrong input" \
-                                        --msgbox "\nNothing selected :-(. \n\nPlease try again" 15 100
-                                        inputok="false"
-                                else
-                                        inputok="true"
-                        fi
-                done
+                tv_cardreader="None"       
 fi
