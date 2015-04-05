aafa9f4cc3890bd0ea06c15a2fcc6157ce05216d
Paul Krause <github@uk3k.de>
2015-04-05 12:26:41 +0200

    Update pyload_select.sh

--- dialog_scripts/additional/pyload_select.sh
+++ dialog_scripts/additional/pyload_select.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-#select additional stuff
+#select poyload to install
+
+input=`whiptail --backtitle "$headline" \
+            --title "Install Pyload" \
+            --yesno "\nDo you want to install the Pyload Download Manager? \n\n " 15 100\`
+    if [ "$input" = "0" ]
+      then
+        add_pyload="true"
+      else
+        add_pyload="false"
+    fi
