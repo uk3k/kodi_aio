ce7d3cf35d3f3fee4e159b5f24f0f2a942e7a4b4
Paul Krause <github@uk3k.de>
2015-04-05 11:38:12 +0200

    Update vdr_select.sh

--- dialog_scripts/live_tv/vdr_select.sh
+++ dialog_scripts/live_tv/vdr_select.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#enable live-tv support
+
+input=`whiptail --backtitle "$headline" \
+        --title "Enable Live-TV" \
+        --yesno "\nInstall VDR to enable Live-TV support and tv-recording features? \n\n " 15 100 2 \`
+if [ "$input" = "0" ]
+  then
+    tv_vdr="true"
+  else
+    tv_vdr="false"
+fi
