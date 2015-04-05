0defa37771575dea001ea360df2a2c0cb6916f5c
Paul Krause <github@uk3k.de>
2015-04-05 11:51:05 +0200

    Update dvb-type_select.sh

--- dialog_scripts/live_tv/dvb-type_select.sh
+++ dialog_scripts/live_tv/dvb-type_select.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+#select dvb-type for system
+
+if [ "$tv_vdr" = "true" ]
+  then
+    input=`whiptail --backtitle "$headline" \
+        --title "Setup mode" \
+        --menu "\nSelect your broadcasting system/TV-Card type: \n\n " 30 100 2 \
+                "DVB-T"       ""	\
+                "DVB-S"       ""	\
+                "DVB-C"       ""  3>&1 1>&2 2>&3`
+    case $input in
+      DVB-T)  tv_dvb_type="T";;
+      DVB-S)  tv_dvb_type="T";;
+      DVB-C)  tv_dvb_type="C";;
+    esac
+fi
