5e7399a6b851ac5c1ea56fdc81446dec772860f0
Paul Krause <github@uk3k.de>
2015-04-05 12:07:25 +0200

    Update precfg_oscam_select.sh

--- dialog_scripts/live_tv/precfg_oscam_select.sh
+++ dialog_scripts/live_tv/precfg_oscam_select.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 #use predefined settings for paytv provider
+
+if [ "$tv_vdr" = "true" ] && [ "$tv_oscam" = "true" ]
+  then
+    then
+      input=`whiptail --backtitle "$headline" \
+          --title "Pay-TV Provider" \
+          --menu "\nSelect Pay-/HD-TV provider \n\n " 30 100 2 \
+                  "UM"        "Unity Media"	        \
+                  "KBW"       "KabelBW"	            \
+                  "KD"        "Kabel Deutschland"   \
+                  "HD Plus"   "HD+"                 \
+                  "SKY"       "SKY Deutschland" 3>&1 1>&2 2>&3`
+      case $input in
+        UM)       tv_provider="UM"      ;;
+        KBW)      tv_provider="KBW"     ;;
+        KD)       tv_provider="KD"      ;;
+        HD Plus)  tv_provider="HDPlus"  ;;
+        SKY)      tv_provider="SKY"     ;;
+      esac
+  else
+    tv_provider="None"
+fi
