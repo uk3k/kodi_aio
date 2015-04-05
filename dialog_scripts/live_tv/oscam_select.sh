407b0f91337bb6c6adf8b836b99748f6992078c2
Paul Krause <github@uk3k.de>
2015-04-05 11:56:08 +0200

    Update oscam_select.sh

--- dialog_scripts/live_tv/oscam_select.sh
+++ dialog_scripts/live_tv/oscam_select.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#enable pay-tv descrambling (OSCAM)
+
+if [ "$tv_vdr" = "true" ]
+  then
+    input=`whiptail --backtitle "$headline" \
+            --title "Install OCAM" \
+            --yesno "\nInstall OSCAM to descramble Pay-/HD-TV? \n\n " 15 100 2 \`
+    if [ "$input" = "0" ]
+      then
+        tv_oscam="true"
+    fi
+  else
+    tv_oscam="false"
+fi
