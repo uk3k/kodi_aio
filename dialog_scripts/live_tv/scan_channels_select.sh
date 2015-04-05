7858ce86bc7d04a98dd5a0474bffa4e431385e09
Paul Krause <github@uk3k.de>
2015-04-05 12:24:35 +0200

    Update scan_channels_select.sh

--- dialog_scripts/live_tv/scan_channels_select.sh
+++ dialog_scripts/live_tv/scan_channels_select.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+#scan for available TV-Channels
+
+if [ "$tv_vdr" = "true" ] && [ "$tv_dvb_type" != "None" ] && [ "$tv_prompt_scan" = "true" ]
+  then
+    input=`whiptail --backtitle "$headline" \
+          --title "Scan for TV-Channels" \
+          --yesno "\nDo you want to automatically scan for all available TV-Channels? \n\n " 15 100 \`
+    if [ "$input" = "0" ]
+      then
+        tv_scan="true"
+    fi
+  else
+    tv_scan="false"
+fi
