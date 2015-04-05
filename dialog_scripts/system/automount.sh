1f9bed8c1dc53691a8208916cfc5c6a28dc79b00
Paul Krause <github@uk3k.de>
2015-04-05 11:43:50 +0200

    Create automount.sh

---
+++ dialog_scripts/system/automount.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#1. try automounting all local filesystems
+#2. search for media files
+#3. ask for automounting --> create share for samba and fstab
+#4. ask for adding as kodi-source
