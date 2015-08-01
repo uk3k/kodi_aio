dd720755d599e14becc5b43f913b2e3063b8a96a
Paul Krause <github@uk3k.de>
2015-08-01 13:09:13 +0200

    bugfix

--- dialog_scripts/live_tv/vdr_select.sh
+++ dialog_scripts/live_tv/vdr_select.sh
@@ -2,10 +2,11 @@
 #enable live-tv support
 
 if (whiptail --backtitle "$headline" \
-        --title "Enable Live-TV" \
-        --yesno "\nInstall VDR to enable Live-TV support and tv-recording features? \n\n " 15 100)
- then
-    tv_vdr="true"
-  else
-    tv_vdr="false"
+            --title "Install Pyload" \
+            --yesno "\nDo you want to install the Pyload Download Manager? \n\n " 15 100)
+      then
+        add_pyload="true"
+      else
+        add_pyload="false"
 fi
+
