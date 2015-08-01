f39b79cec3c24228a81ade7c7c131a359c6fd8d5
Paul Krause <github@uk3k.de>
2015-08-01 12:30:15 +0200

    Update card-reader_detection.sh

--- hw_detect/card-reader_detection.sh
+++ hw_detect/card-reader_detection.sh
@@ -5,7 +5,12 @@
 
 if [ $tv_cardreader = "Smargo" ] 
   then
-    tv_cr_busid=$(lsusb | grep "Future Technology" | awk -v OFS=':' '{print $2,$4}' | sed s/://2) 
+    if [ $"(lsusb | grep "Future Technology")" ]
+      then
+        tv_cr_busid=$(lsusb | grep "Future Technology" | awk -v OFS=':' '{print $2,$4}' | sed s/://2)
+      else
+        tv_cr_busid="000:000"
+    fi
   else
     tv_cr_busid="000:000"
 fi
