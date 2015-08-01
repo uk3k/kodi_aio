7334c887d106176d6e5557aed79f222468bc88fc
Paul Krause <github@uk3k.de>
2015-08-01 13:14:43 +0200

    Update oscam_select.sh

--- dialog_scripts/live_tv/oscam_select.sh
+++ dialog_scripts/live_tv/oscam_select.sh
@@ -8,9 +8,12 @@ if [ "$tv_vdr" = "true" ]
             --yesno "\nInstall OSCAM to descramble Pay-/HD-TV? \n\n " 15 100)
       then
         tv_oscam="true"
+        tv_precfg_oscam="true"
       else
         tv_oscam="false"
+        tv_precfg_oscam="false"
     fi
   else
     tv_oscam="false"
+    tv_precfg_oscam="false"
 fi
