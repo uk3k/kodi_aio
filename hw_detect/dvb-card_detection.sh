d67101f02cb7105e4c26948b37a5a243cc8a417e
Paul Krause <github@uk3k.de>
2015-08-01 12:46:51 +0200

    Update dvb-card_detection.sh

--- hw_detect/dvb-card_detection.sh
+++ hw_detect/dvb-card_detection.sh
@@ -5,18 +5,4 @@
 if [ -n "$(ls /dev/dvb/ | grep ^adapter.*)" ]
   then
     tv_vdr="true" 
-    tv_oscam="true"
-    tv_prefcg_oscam="true"
-    tv_provider="KBW"
-    tv_cardreader="Smargo"
-    tv_prompt_scan="true"
-  else
-    tv_vdr="false" 
-    tv_oscam="false"
-    tv_prompt_scan="false"
-    tv_prefcg_oscam="true"
-    tv_provider="None"
-    tv_cardreader="None"
-    tv_prompt_scan="false"
-    tv_scan="false"
 fi
