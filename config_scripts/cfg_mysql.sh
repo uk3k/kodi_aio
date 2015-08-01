341fd224e7e51c5975ad04e0ca92240b9391ddc6
Paul Krause <github@uk3k.de>
2015-08-01 13:58:05 +0200

    Update cfg_mysql.sh

--- config_scripts/cfg_mysql.sh
+++ config_scripts/cfg_mysql.sh
@@ -2,9 +2,9 @@
 ###tweaks for mysql
 
 #add user kodi to mysql and grant privileges
-mysql -u root --password=$mysql_passwd -e "CREATE USER 'kodi'@'%' IDENTIFIED BY 'kodi'"
-mysql -u root --password=$mysql_passwd -e "GRANT ALL ON *.* TO 'kodi'@'%'"
+mysql -u root --password=$sql_rootpw -e "CREATE USER 'kodi'@'%' IDENTIFIED BY '$sql_userpw'"
+mysql -u root --password=$sql_rootpw -e "GRANT ALL ON *.* TO 'kodi'@'%'"
 
 #update my.cnf to allow connections from the local net
-replace="s/bind-address*.*/bind-address = $mysql_host/" 
+replace="s/bind-address*.*/bind-address = $sql_ip/" 
 sed -i -e "$replace" /etc/mysql/my.cnf
