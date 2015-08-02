6c9ef057f766f92818248165dd8926879ed7626e
Paul Krause <github@uk3k.de>
2015-08-02 16:45:53 +0200

    Update vdr_select.sh

--- dialog_scripts/live_tv/vdr_select.sh
+++ dialog_scripts/live_tv/vdr_select.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 #enable live-tv support
 
-if (whiptail --backtitle "$headline" \
-            --title "Install VDR" \
-            --yesno "\nDo you want to install Live-TV support? \n\n " 15 100)
-      then
-        tv_vdr="true"
-        tv_prompt_scan="true"
-      else
-        tv_vdr="false"
-        tv_prompt_scan="false"
-fi
+if [ "$tv_card_present" = "true" ]
+            then        
+                        if (whiptail --backtitle "$headline" \
+                                    --title "Install VDR" \
+                                    --yesno "\nDo you want to install Live-TV support? \n\n " 15 100)
+                                    then
+                                                tv_vdr="true"
+                                                tv_prompt_scan="true"
+                                    else
+                                                tv_vdr="false"
+                                                tv_prompt_scan="false"
+                        fi
+            else
+                        if (whiptail --backtitle "$headline" \
+                                    --title "Install VDR" \
+                                    --yesno "\nNo working TV-Card was detected. \n\nDo you want to install Live-TV support anyway? \n\n " 15 100)
+                                    then
+                                                tv_vdr="true"
+                                                tv_prompt_scan="true"
+                                    else
+                                                tv_vdr="false"
+                                                tv_prompt_scan="false"
+                        fi
 
