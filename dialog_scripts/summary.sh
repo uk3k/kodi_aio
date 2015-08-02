b50f8158334c3a7d8fd834e6f38e4dae6e0d9947
Paul Krause <github@uk3k.de>
2015-08-02 16:57:54 +0200

    Update summary.sh

--- dialog_scripts/summary.sh
+++ dialog_scripts/summary.sh
@@ -3,13 +3,14 @@
 #V1.0.0.0.A
 
 echo "General Information" > /home/kodi/summary.txt
-echo "Bug Reports: .............. github@uk3k.de" >> /home/kodi/summary.txt
-echo "Script-Version: ........... $script_version" >> /home/kodi/summary.txt
-echo "Ubuntu Release: ........... $sys_os" >> /home/kodi/summary.txt
-echo "System type to install: ... $sys_type" >> /home/kodi/summary.txt
-echo "Setup mode: ............... $setup" >> /home/kodi/summary.txt
-echo "Grapics Vendor: ........... $sys_gfx" >> /home/kodi/summary.txt
+echo "Bug Reports: ....................................... github@uk3k.de" >> /home/kodi/summary.txt
+echo "Script-Version: .................................... $script_version" >> /home/kodi/summary.txt
+echo "Ubuntu Release: .................................... $sys_os" >> /home/kodi/summary.txt
+echo "System type to install: ............................ $sys_type" >> /home/kodi/summary.txt
+echo "Setup mode: ........................................ $setup" >> /home/kodi/summary.txt
+echo "Grapics Vendor: .................................... $sys_gfx" >> /home/kodi/summary.txt
 echo "-----------------------------" >> /home/kodi/summary.txt
+echo "" >> /home/kodi/summary.txt
 
 echo "Network Configuration" >> /home/kodi/summary.txt
 echo "Primary Network Interface: ......................... $nw_iface" >> /home/kodi/summary.txt
@@ -24,12 +25,14 @@ echo "Gateway: ........................................... $nw_gateway" >> /home
 echo "Nameserver #1: ..................................... $nw_dns1" >> /home/kodi/summary.txt
 echo "Nameserver #2: ..................................... $nw_dns2" >> /home/kodi/summary.txt
 echo "-----------------------------" >> /home/kodi/summary.txt
+echo "" >> /home/kodi/summary.txt
 
 echo "Mysql Settings" >> /home/kodi/summary.txt
 echo "Mysql-Server-IP: ................................... $sql_ip" >> /home/kodi/summary.txt
 echo "Mysql Root Password: ............................... $sql_rootpw" >> /home/kodi/summary.txt
 echo "Mysql User Password: ............................... $sql_userpw" >> /home/kodi/summary.txt
 echo "-----------------------------" >> /home/kodi/summary.txt
+echo "" >> /home/kodi/summary.txt
 
 echo "Live-TV Selections" >> /home/kodi/summary.txt
 echo "TV-Card detected: .................................. $tv_card_present" >> /home/kodi/summary.txt
@@ -42,10 +45,12 @@ echo "Selected Smart-Card-Reader vendor: ................. $tv_cardreader" >> /h
 echo "Bus:ID of your Smart-Card-Reader: .................. $tv_cr_busid (000:000=dummy)" >> /home/kodi/summary.txt
 echo "Scan for available TV-Channels: .................... $tv_scan" >> /home/kodi/summary.txt
 echo "-----------------------------" >> /home/kodi/summary.txt
+echo "" >> /home/kodi/summary.txt
 
 echo "Additional Packages" >> /home/kodi/summary.txt
 echo "Install Pyload Download-Manager: ................... $add_pyload" >> /home/kodi/summary.txt
 echo "-----------------------------" >> /home/kodi/summary.txt
+echo "" >> /home/kodi/summary.txt
 
 clear
 #debug
