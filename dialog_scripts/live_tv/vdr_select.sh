09bfd15655690c02d1073b2c9f3df19a1ff9e7f8
Paul Krause <github@uk3k.de>
2015-08-02 16:32:29 +0200

    Update vdr_select.sh

--- dialog_scripts/live_tv/vdr_select.sh
+++ dialog_scripts/live_tv/vdr_select.sh
@@ -6,7 +6,9 @@ if (whiptail --backtitle "$headline" \
             --yesno "\nDo you want to install Live-TV support? \n\n " 15 100)
       then
         tv_vdr="true"
+        tv_prompt_scan="true"
       else
         tv_vdr="false"
+        tv_prompt_scan="false"
 fi
 
